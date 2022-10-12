c7298e427c78b9deae64180191044601d0a82ff4
Mykyta Medvediev <arlekin8881@gmail.com>
2022-10-12 14:00:37 -0700

    Changed DOM for easeir styling

--- src/components/body/main-body.js
+++ src/components/body/main-body.js
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import Personal from './personal'
-import ExperienceList from './experience'
-import EducationList from './education'
-import Resume from './resume'
-import uniqid from 'uniqid'
+import { useState } from 'react';
+import Personal from './personal';
+import ExperienceList from './experience';
+import EducationList from './education';
+import Resume from './resume';
+import uniqid from 'uniqid';
 
 export default function Body(props) {
     const [personal, setPersonal] = useState({
@@ -14,7 +14,7 @@ export default function Body(props) {
         phone: '',
         email: '',
         description: '',
-    })
+    });
     const [experience, setExperience] = useState([
         {
             id: uniqid(),
@@ -24,7 +24,7 @@ export default function Body(props) {
             from: '',
             to: '',
         },
-    ])
+    ]);
     const [education, setEducation] = useState([
         {
             id: uniqid(),
@@ -34,37 +34,37 @@ export default function Body(props) {
             from: '',
             to: '',
         },
-    ])
+    ]);
 
     const onInput = (e) => {
-        console.log({personal, experience, education})
+        console.log({ personal, experience, education });
         if (e.target.parentNode.className === 'personal') {
             setPersonal(
                 Object.assign({}, personal, {
                     [e.target.id]: e.target.value,
                 })
-            )
+            );
         } else if (e.target.parentNode.className === 'experience') {
-            setExperience((prevExperience) => (
+            setExperience((prevExperience) =>
                 prevExperience.map((obj) =>
-                obj.id === e.target.parentNode.id
-                    ? { ...obj, [e.target.className]: e.target.value }
-                    : obj
+                    obj.id === e.target.parentNode.id
+                        ? { ...obj, [e.target.className]: e.target.value }
+                        : obj
                 )
-            ))
+            );
         } else if (e.target.parentNode.className === 'education') {
-            setEducation((prevEducation) => (
+            setEducation((prevEducation) =>
                 prevEducation.map((obj) =>
-                obj.id === e.target.parentNode.id
-                    ? { ...obj, [e.target.className]: e.target.value }
-                    : obj
+                    obj.id === e.target.parentNode.id
+                        ? { ...obj, [e.target.className]: e.target.value }
+                        : obj
                 )
-            ))
+            );
         }
-    }
+    };
 
     const addExperience = (e) => {
-        let newExperience = [...experience]
+        let newExperience = [...experience];
         newExperience.push({
             id: uniqid(),
             position: '',
@@ -72,12 +72,12 @@ export default function Body(props) {
             city: '',
             from: '',
             to: '',
-        })
-        setExperience(newExperience)
-    }
+        });
+        setExperience(newExperience);
+    };
 
     const addEducation = (e) => {
-        let newEducation = [...education]
+        let newEducation = [...education];
         newEducation.push({
             id: uniqid(),
             school: '',
@@ -85,40 +85,44 @@ export default function Body(props) {
             city: '',
             from: '',
             to: '',
-        })
-        setEducation(newEducation)
-    }
+        });
+        setEducation(newEducation);
+    };
 
     const deleteExperience = (e) => {
         setExperience(
             experience.filter((obj) => obj.id !== e.target.parentNode.id)
-        )
-    }
+        );
+    };
 
     const deleteEducation = (e) => {
         setEducation(
             education.filter((obj) => obj.id !== e.target.parentNode.id)
-        )
-    }
+        );
+    };
 
     return (
         <>
-            <Personal onInput={onInput} />
-            <div id="experience-section">
-                <ExperienceList
-                    onInput={onInput}
-                    onAdd={addExperience}
-                    onDelete={deleteExperience}
-                    items={experience}
-                />
-            </div>
-            <div id="education-section">
-                <EducationList
-                    onInput={onInput}
-                    onAdd={addEducation}
-                    onDelete={deleteEducation}
-                    items={education}
-                />
+            <div id="filling-section">
+                <div id="personal-section">
+                    <Personal onInput={onInput} />
+                </div>
+                <div id="experience-section">
+                    <ExperienceList
+                        onInput={onInput}
+                        onAdd={addExperience}
+                        onDelete={deleteExperience}
+                        items={experience}
+                    />
+                </div>
+                <div id="education-section">
+                    <EducationList
+                        onInput={onInput}
+                        onAdd={addEducation}
+                        onDelete={deleteEducation}
+                        items={education}
+                    />
+                </div>
             </div>
             <div id="resume-section">
                 <Resume
@@ -128,5 +132,5 @@ export default function Body(props) {
                 />
             </div>
         </>
-    )
+    );
 }
